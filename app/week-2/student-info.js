034dacef5bb593d8e349e09b8f8499c83b9d4ce1
mfosato <marybethsato@yahoo.com>
2024-09-24 17:32:00 -0600

    feat: week 2 assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+    return (
+        <div>
+            <p>Marybeth Flonia Sato</p>
+            <Link
+                href="https://github.com/marybethsato"
+            >
+                https://github.com/marybethsato
+            </Link>
+
+        </div>
+
+    );
+}
